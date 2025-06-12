45537c7469b04221d645169652b25db21bb83f7a
Kareem <hi@kareemalbaba.com>
2025-06-12 11:46:19 -0400

    Lecture 8

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/InvPlayerController.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/InvPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+void AInvPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* Context : DefaultMappingContexts)
+		{
+			Subsystem->AddMappingContext(Context, 0);
+		}
+	}
+}
+
+void AInvPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &AInvPlayerController::PrimaryInteract);
+}
+
+void AInvPlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Warning, TEXT("PrimaryInteract"));
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/InvPlayerController.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InvPlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInvPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void PrimaryInteract();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory|Input")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultMappingContexts;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory|Input")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+};
